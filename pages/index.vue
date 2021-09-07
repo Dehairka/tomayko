8f039fa2925cfacf6b7adf16b420e9e31a5f39fe
Dehairka <esteve.charly@gmail.com>
2021-09-07 10:14:20 +0200

    add herobanner

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="homePage">
-    <h1>Hello World !</h1>
+    <div class="homePage_herobanner">
+      <img :src="require('@/assets/img/herobanner.png')" alt="herobanner image">
+      <div class="content">
+        <h1>New</h1>
+        <h1>collection !</h1>
+        <input class="btn" type="button" value="Discover">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+.homePage{
+  &_herobanner{
+    background-color: grey;
+    position: relative;
+
+    img{
+      width: 100vw;
+      height: 70vh;
+      object-fit: cover;
+    }
+    .content{
+      position: absolute;
+      bottom: 0;
+      padding: 16px;
+      color: white;
+      font-size: 1.6em;
+      font-weight: 600;
+    }
+  }
+}
+</style>
